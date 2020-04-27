d7d89e73460a38fc345f1639af3a00ba5d7d8d85
ddarashkou <ddarashkou@exadel.com>
2020-04-27 19:51:56 +0300

    feat(lesson-9): lesson sources

---
+++ lesson-9/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from "rxjs";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+// ^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$
+}
