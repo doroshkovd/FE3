c198c4cac587863e0516f5b8884129a103721bd1
ddarashkou <ddarashkou@exadel.com>
2020-04-28 13:17:03 +0300

    feat(lesson-10): lesson sources

---
+++ lesson-10/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "./shared/services/user.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit(): void {
+    this.userService.getUsers()
+      .subscribe((users) => users);
+  }
+
+}
