99eaed86a5a1b4182d5e271f382f2c615ffd44e3
ddarashkou <ddarashkou@exadel.com>
2020-05-19 21:12:25 +0300

    feat(lesson-14): lesson sources

---
+++ lesson-14/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Observable, of } from "rxjs";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  constructor() {
+  }
+
+}
