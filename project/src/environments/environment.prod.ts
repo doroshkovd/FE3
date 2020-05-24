b2c297863b699706733324f640802d62c155ad5a
ddarashkou <ddarashkou@exadel.com>
2020-05-24 14:04:11 +0300

    feat(project): lesson sources

--- project/src/environments/environment.prod.ts
+++ project/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  api: 'https://fe3-course.aws.com',
+  api: 'https://fe3-course.firebaseio.com',
+  apiKey: 'AIzaSyAozAf0FtdGcdZzr25vRIsuITwll-eldpE',
+  signUpUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
+  loginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
+  authRedirectUrl: '/cars',
 };
